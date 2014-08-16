703b21379ef7b1f3566b10da1d999ba2ff786082
Alex Korotkov <durm.icecoffee@gmail.com>
2014-08-16 13:00:05 +0400

    venev

--- aksvrnru/wsgi.py
+++ aksvrnru/wsgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+import sys
+sys.path.insert(0, "/home/durm/envs/aksvrn/lib/python2.7/site-packages")
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aksvrnru.settings")
 
