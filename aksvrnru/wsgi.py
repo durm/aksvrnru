44e93d696156acf50423e692819ed725f61f5290
Alex Korotkov <durm.icecoffee@gmail.com>
2014-08-16 13:09:32 +0400

    venev

--- aksvrnru/wsgi.py
+++ aksvrnru/wsgi.py
@@ -1,14 +1,3 @@
-#!/home/durm/envs/aksvrn/bin/python
-
-"""
-WSGI config for aksvrnru project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
 import sys
 sys.path.insert(0, "/home/durm/envs/aksvrn/lib/python2.7/site-packages")
 
