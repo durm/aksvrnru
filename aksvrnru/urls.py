b77f1180f6433ae72167636148f83e19c022f759
Korotkov Alex <durm.icecoffee@gmail.com>
2014-08-19 19:09:45 +0400

    html2text

--- aksvrnru/urls.py
+++ aksvrnru/urls.py
@@ -8,7 +8,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'aksvrnru.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
