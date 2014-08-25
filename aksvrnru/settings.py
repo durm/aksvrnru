d61638c19038c826b3315962d1a10e6ac40ec48f
Korotkov Alex <durm.icecoffee@gmail.com>
2014-08-25 15:02:47 +0400

    login auth me

--- aksvrnru/settings.py
+++ aksvrnru/settings.py
@@ -118,3 +118,7 @@ STATICFILES_FINDERS = (
 STATICFILES_DIRS = (
 os.path.join(BASE_DIR, "static"),
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+)
--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^rubrics/hierarchy/$', 'products.views.rubrics_hierarchy', name='rubrics_hierarchy'),
+    url(r'^login_page/$', 'products.views.login_page', name='login_page'),
+    url(r'^login/$', 'products.views.login_proc', name='login'),
+    url(r'^me/$', 'products.views.me', name='me'),
 )
