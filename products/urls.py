53966d8e656ea37c907d299de17b1a04d69a4b6f
Korotkov Alex <durm.icecoffee@gmail.com>
2014-08-29 17:01:58 +0400

    product

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^rubrics/hierarchy/$', 'products.views.rubrics_hierarchy', name='rubrics_hierarchy'),
+    url(r'^product(?P<num>[0-9]+)/$', 'products.views.get_product', name='get_product'),
 )
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 from django.shortcuts import redirect, render_to_response
-from products.models import Rubric
+from products.models import Rubric, Product
 from pages.views import get_context
 from django.contrib.auth import authenticate
 from aksvrnru.views import error
@@ -12,3 +12,13 @@ from django.contrib.auth import authenticate, login
 def rubrics_hierarchy(request):
     rubrics = Rubric.objects.filter(parent__isnull=True)
     return render_to_response('hierarchy/hierarchy.html', {"rubrics":rubrics}, get_context(request))
+
+def get_product(request, num):
+    try:
+        product = Product.objects.get(id=num)
+        return render_product(request, product)
+    except Product.DoesNotExist :
+        return page_doesnot_exist(request, num)
+    
+def render_product(request, product):
+    return render_to_response("products/product.html", {'product':product}, get_context(request))
