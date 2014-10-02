a04d5d683c14f0a7c9ef661d86434b24d132612e
Korotkov Alex <durm.icecoffee@gmail.com>
2014-10-02 13:49:36 +0400

    locale

--- aksvrnru/wsgi.py
+++ aksvrnru/wsgi.py
@@ -6,8 +6,5 @@ sys.path.insert(0, "/home/durm/envs/aksvrn/lib/python2.7/site-packages")
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aksvrnru.settings")
 
-import locale
-locale.setlocale(locale.LC_TIME,'en_US.utf8')
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
