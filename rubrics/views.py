034027e72b0771c834d2193b24f90d3adc82eda7
Alex Korotkov <durm.icecoffee@gmail.com>
2014-11-25 19:40:52 +0300

    rubrics

--- rubrics/views.py
+++ rubrics/views.py
@@ -18,5 +18,9 @@ def rubricator(request):
             rubric = Rubric.objects.get(id=rubric_id)
         except Rubric.DoesNotExist :
             return error(request, u"Нет рубрики с таким идентификатором!", u"")
-    parents_id = get_parents_id(rubric, [])
-    return render_to_response("rubrics/rubrics.html", {'main': rubric, 'rubrics':rubrics, 'parents_id':parents_id}, get_context(request))
+    rubricator_path = []
+    parents_id = []
+    if rubric is not None :
+        rubricator_path = rubric.rubricator_path()
+        parents_id = map(lambda r: r.id, rubricator_path)
+    return render_to_response("rubrics/rubrics.html", {'main': rubric, 'rubrics':rubrics, 'parents_id':parents_id, 'rubricator_path':rubricator_path}, get_context(request))
