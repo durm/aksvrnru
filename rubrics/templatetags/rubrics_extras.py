1e2257a487cc2bc445f99ccaa7263cd470bd1c8f
Alex Korotkov <durm.icecoffee@gmail.com>
2014-11-26 13:01:38 +0300

    query_transform

--- rubrics/templatetags/rubrics_extras.py
+++ rubrics/templatetags/rubrics_extras.py
@@ -7,11 +7,6 @@ from django.db.models import Q
 
 register = template.Library()
 
-def get_products_count(rubric):
-    all_children = rubric.get_all_published_children()
-    products = Product.get_published_products().filter(Q(rubrics__in=all_children)|Q(rubrics__in=[rubric]))
-    return len(products)
-    
 @register.simple_tag
 def query_transform(request, **kwargs):
     updated = request.GET.copy()
