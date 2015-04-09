9d50ea67b074d672c5b87e2497fa7bb4aa8799e7
korotkov <durm@linux.site>
2015-04-09 16:59:46 +0300

    create rubricator

--- rubrics/urls.py
+++ rubrics/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^rubricator/$', 'rubrics.views.rubricator', name='rubricator'),
     #
-    url(r'^/rubricator/create/$', 'rubrics.views.create_rubricator_from_file', name='create_rubricator_from_file'),
-    url(r'^/rubricator/create/do/$', 'rubrics.views.do_create_rubricator_from_file', name='do_create_rubricator_from_file'),
+    url(r'^rubricator/create/$', 'rubrics.views.create_rubricator_from_file', name='create_rubricator_from_file'),
+    url(r'^rubricator/create/do/$', 'rubrics.views.do_create_rubricator_from_file', name='do_create_rubricator_from_file'),
 )
--- rubrics/views.py
+++ rubrics/views.py
@@ -9,19 +9,23 @@ from django.db.models import Q
 import types
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from rubrics.taxparser import parse_txt_tax
+from django.template.context_processors import csrf
+from django.core.urlresolvers import reverse
 
 def get_parents_id(rubric, ids):
     if rubric is None : return ids
     return get_parents_id(rubric.parent, ids + [rubric.id])
     
 def create_rubricator_from_file(request):
-    return render_to_response("rubrics/create_rubricator_from_file.html")
+    c = {}
+    c.update(csrf(request))
+    return render_to_response("rubrics/create_rubricator_from_file.html", c)
 
 def do_create_rubricator_from_file(request):
     rubricator = request.FILES["rubricator"].readlines()
-    tax = parse_txt_tax(rubricator)
+    tax = parse_txt_tax(map(lambda x: x.decode("utf-8"),rubricator))
     Rubric.create_from_xml(tax)
-    return redirect("/rubricator/create/")
+    return redirect(reverse("create_rubricator_from_file"))
     
 def rubricator(request):
     rubric_id = request.GET.get("rubric", None)
